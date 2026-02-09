e4ab0bedc95545e5bb05cabf271aa3592a8f264a
Sanvi <qwertyusdfgh@gmail.com>
2026-02-09 10:37:45 +0530

    Added

---
+++ ascii.c
@@ -0,0 +1,87 @@
+// #include <stdio.h>
+// #include <string.h>
+// int main(){
+    // ans 1
+    // int sum=0;
+    // char a[]="_._Self_Lover_._";
+    // for (int i=0;i<strlen(a);i++){
+    //     if ((65<=a[i] && a[i]<=90) || (97<=a[i] && a[i]<=122)){
+    //         sum+=1;
+    //     }
+    // }
+    // printf("%d",sum);
+
+    // ans 2
+    // int s=0;
+    // char b[]="Innocent_Guy";
+    // for (int i=0;i<strlen(b);i++){
+    //     if (65<=b[i] && b[i]<=90){
+    //         s+=1;
+    //     }
+    // }
+    // printf("%d",s);
+
+    // ans 3
+    // char c[]="HeArTsTeAlEr";
+    // for (int i=0;i<strlen(c);i++){
+    //     if (c[i]<=90 && c[i]>=65){
+    //         printf("%c",c[i]+32);
+    //     }else{
+    //         printf("%c",c[i]-32);
+    //     }
+    // }
+    // return 0;
+
+    // ans 4
+    // char d[]="_._MrInDiA";
+    // for (int i=0;i<strlen(d);i++){
+    //     if(d[i]<=90 && d[i]>=65){
+    //         printf("%c",d[i]+32);
+    //     }else if(d[i]>=97&&d[i]<=122){
+    //         printf("%c",d[i]-32);
+    //     }else{
+    //         printf("%c",d[i]);
+    //     }
+    // }
+
+    // ans 5
+    // int sum=0;
+    // char e[]="Dad_Little_Princess";
+    // for (int i=0;i<strlen(e);i++){
+    // if (97<=e[i] && e[i]<=122){
+    //         sum+=1;
+    //     }
+    // }
+    // printf("%d",sum);
+
+    // ans 6
+    // char f[]="LiFe~SaVeR";
+    // for (int i=(strlen(f)-1);i>=0;i--){
+    //     if(f[i]<=90 && f[i]>=65){
+    //         printf("%c",f[i]+32);
+    //     }else if(f[i]>=97 && f[i]<=122){
+    //         printf("%c",f[i]-32);
+    //     }else{
+    //         printf("%c",f[i]);   
+    // }}
+
+    // ans 7
+    // char g[]="Dusky_Queen1589";
+    // for (int i=(strlen(g)-1);i>=0;i--){
+    //     if(g[i]<=57 && g[i]>=48){
+    //         printf("%c",g[i]);}}
+
+    // ans 8
+//     int sum_s =0;
+//     int sum_b = 0;
+//     char h[]="LoVeR_bOy";
+//     for (int i=0;i<strlen(h);i++){
+//         if(h[i]<=90 && h[i]>=65){
+//             sum_b+=1;
+//         }else if(h[i]>=97 && h[i]<=122){
+//             sum_s+=1;
+//         }
+//     }
+//     printf("small: %d,big: %d",sum_s,sum_b);
+// return 0;
+// }
---
+++ loop.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+// int main(){
+//     int sum=0;
+//     for(int i=1;i<=10;i++){
+//         sum+=i;
+//     }
+//     printf("%d",sum);
+// }
+
+
+
+//ques1 list nos from 21-40 without nos divisible by 3
+// int main(){
+//     for(int i=21;i<=40;i++){
+//         if (i%3==0){
+//             continue;
+//         }
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+//ques2 sum of all nos which is div by 8 and 7 from 1 to 500
+// int main(){
+//     int sum=0;
+//     for(int i=1;i<=500;i++){
+//         if ((i%7!=0) || (i%8!=0)){
+//             continue;
+//         }else{
+//             sum+=i;
+//         }
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+
+//ques 3 diff btw sum of all nos from 1-50 and sum of even nos from 51 to 100
+// int main(){
+//     int sum1=0;
+//     int sum2=0;
+//     int diff;
+//     for(int i=1;i<=50;i++){
+//         sum1+=i;
+//         }
+    
+//     for(int i=51;i<=100;i++){
+//         if (i%2==0){
+//             sum2+=i;
+//         }else{
+//             continue;
+//         }
+//     }
+//     diff=sum2-sum1;
+//     printf("sum1: %d\n",sum1);
+//     printf("sum2: %d\n",sum2);
+//     printf("diff: %d",diff);
+//     return 0;
+// }
+
+//ques 4 avg from 31-40 except 33,36,39
+// int main(){
+//     int sum=0;
+//     float avg;
+//     int total=0;
+//     for(int i=31;i<=40;i++){
+//         if (i==33||i==36||i==39){
+//             continue;
+//         }else{
+//             sum+=i;
+//             total+=1;
+//         }
+//     }
+//     printf("sum: %d\n",sum);
+//     avg = sum/total;
+//     printf("avg: %.2f",avg);
+//     return 0;
+// }
+
+// ques 5 infinite loop
+// int main(){
+//     int a;
+//     while(1){
+//         scanf("%d",&a);
+//         if (a==-1){
+//             printf("Program ended");
+//             break;
+//         }
+//     }
+//     return 0;
+// }
+
+// // ques 6 1-100, skip /3 at 75 stop and print 75 as well
+//  int main(){
+//     for(int i=1;i<=100;i++){
+//         if (i==75){
+//             printf("%d",75);
+//             break;
+//         }
+//         if (i%3==0){
+//             continue;
+//         }
+        
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// ques 7 1-50, prodect of /9,>40 stop:
+int main(){
+    int prod=1;
+    for(int i=1;i<=50;i++){
+        if (i>40){
+            break;
+        }
+        if (i%9==0){
+            prod*=i;
+        }
+    }
+    printf("%d",prod);
+    return 0;
+}
