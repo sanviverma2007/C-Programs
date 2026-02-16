dea6c25e2dbcdeb8d99cf6ca7fac9f17e9644332
Sanvi <qwertyusdfgh@gmail.com>
2026-02-16 11:02:08 +0530

    a

--- playwithnos.c
+++ playwithnos.c
@@ -38,77 +38,83 @@
 // }
 
 // pallendrome checker
-#include <stdio.h>
-int main(){
-    int a = 12121;
-    int og = a;
-    int reverse=0;
-    int mod;
-    while (a>0){
-        int mod= a%10;
-        reverse=reverse*10+mod;
-        a = a/10;
-    }
-    if(reverse==og){
-        printf("Pallendrome number");
-    }else{
-        printf("Not a Pallendrome number");
-    }
-}
+// #include <stdio.h>
+// int main(){
+//     int a = 156;
+//     int og = a;
+//     int reverse=0;
+//     int mod;
+//     while (a>0){
+//         int mod= a%10;
+//         reverse=reverse*10+mod;
+//         a = a/10;
+//     }
+//     if(reverse==og){
+//         printf("Pallendrome number");
+//     }else{
+//         printf("Not a Pallendrome number");
+//     }
+// }
 
-// armstrong number 
+// armstrong number checker
 // #include <stdio.h>
 // #include <math.h>
 // int main(){
 //     int n=153;
 //     int og = n;
 //     int sum=0;
+//     int temp, digits = 0;
+//     temp = n;
+//     while(temp > 0){
+//         digits++;
+//         temp = temp / 10;
+//     }
 //     while (n>0){
 //         int mod= n%10;
-//         sum = sum+mod*mod*mod;
+//         sum = sum+(int)(pow(mod,digits)+0.5);
 //         n = n/10;
 //     }
 //     printf("sum is %d and number is %d\n",sum,og);
 //     if(sum==og){
-//         printf("Armstrong NUmber");
+//         printf("Armstrong Number");
 //     }else{
 //         printf("Not an armstrong number");
 //     }
 // }
 
 
-// largest among the sum of even digits and sum of odd digits if a numbers
-// #include <stdio.h>
-// int main(){
-//     int n=2578;
-//     int sum1=0;
-//     int sum2=0;
-//     while (n>0){
-//         int mod= n%10;
-//         if (mod%2!=0){
-//         sum1 = sum1+mod;
-//         }else{
-//             sum2=sum2+mod;
-//         }
-//         n = n/10;
-//     }
-//     if(sum1>sum2){
-//         printf("Sum of odd nos is %d and is greater",sum1);
-//     }else{
-//         printf("Sum of odd nos is %d and is greater",sum2);
-//     }
-// }
+// // largest among the sum of even digits and sum of odd digits if a numbers
+// // #include <stdio.h>
+// // int main(){
+// //     int n=2578;
+// //     int sum1=0;
+// //     int sum2=0;
+// //     while (n>0){
+// //         int mod= n%10;
+// //         if (mod%2!=0){
+// //         sum1 = sum1+mod;
+// //         }else{
+// //             sum2=sum2+mod;
+// //         }
+// //         n = n/10;
+// //     }
+// //     if(sum1>sum2){
+// //         printf("Sum of odd nos is %d and is greater",sum1);
+// //     }else{
+// //         printf("Sum of odd nos is %d and is greater",sum2);
+// //     }
+// // }
 
-// diff btw the diff of even digits and sum of odd digits if a numbers
-// #include <stdio.h>
-// int main(){
-//     int diff, n=2578;
-//     int sum1=0;
-//     int sum2=0;
-//     while (n>0){
-//         int mod= n%10;
-//         if (mod%2!=0){
-//         sum1 = sum1+mod;
+// // diff btw the diff of even digits and sum of odd digits if a numbers
+// // #include <stdio.h>
+// // int main(){
+// //     int diff, n=2578;
+// //     int sum1=0;
+// //     int sum2=0;
+// //     while (n>0){
+// //         int mod= n%10;
+// //         if (mod%2!=0){
+// //         sum1 = sum1+mod;
 //         }else{
 //             sum2=sum2+mod;
 //         }
@@ -121,3 +127,33 @@ int main(){
 //     }
 //     printf("Diff is %d",diff);
 // }
+
+// 2578 28 57
+#include <stdio.h>
+int main(){
+    int mod,n= 2578;
+    int e=0;
+    int o=0;
+    int revo = 0, reve = 0;
+    while(n>0){
+        mod=n%10;
+        if(mod%2==0){
+            e=e*10+mod;
+        }else{
+            o=o*10+mod;
+        }
+        n = n/10;
+    }
+    while(e > 0){
+        reve = reve * 10 + e % 10;
+        e = e / 10;
+    }
+    while(o > 0){
+        revo = revo * 10 + o % 10;
+        o = o / 10;
+    }
+
+    printf("%d\n",reve);
+    printf("%d",revo);
+    return 0;
+}
