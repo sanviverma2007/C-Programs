56fc0a99197446a937141a15d5923b4d3f120601
Sanvi <qwertyusdfgh@gmail.com>
2026-02-22 12:21:01 +0530

    a

--- first_repeated_digit.c
+++ first_repeated_digit.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
-
 int main() {
-    long long n, temp;
-    int digits[20];
-    int freq[10] = {0};
-    int i = 0, found = 0;
+    long long n, temp1, temp2;
+    int digit1, digit2;
+    int found = 0;
 
+    printf("Enter a number: ");
     scanf("%lld", &n);
-    temp = n;
 
-    // Store digits in array (reverse order)
-    while (temp > 0) {
-        digits[i] = temp % 10;
-        temp = temp / 10;
-        i++;
-    }
+    temp1 = n;
 
-    // Traverse from left to right using while
-    i = i - 1;   // move to last stored digit
+    while (temp1 > 0) {
+        digit1 = temp1 % 10;      // Take last digit
+        temp2 = temp1 / 10;       // Remaining number
 
-    while (i >= 0) {
-        if (freq[digits[i]] == 1) {
-            printf("First repeated digit: %d\n", digits[i]);
-            found = 1;
-            break;
+        while (temp2 > 0) {
+            digit2 = temp2 % 10;
+
+            if (digit1 == digit2) {
+                printf("First repeated digit: %d\n", digit1);
+                found = 1;
+                break;
+            }
+
+            temp2 = temp2 / 10;
         }
-        freq[digits[i]]++;
-        i--;
+
+        if (found == 1)
+            break;
+
+        temp1 = temp1 / 10;
     }
 
-    if (!found) {
-        printf("No repeated digits found.\n");
-    }
+    if (found == 0)
+        printf("No repeated digit found.\n");
 
     return 0;
 }
